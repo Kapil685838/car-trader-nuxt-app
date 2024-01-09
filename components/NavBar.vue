7a31014210ef031fca5978da5e514a61c6950ccc
Kapil Chaudhary <kc685838@gmail.com>
2024-01-10 00:57:41 +0530

    Added: NavBar component & default layout

---
+++ components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="sticky top-0 z-50 border-b bg-white shadow-md">
+        <nav class="max-w-7xl mx-auto flex justify-between items-center space-x-1 p-4">
+            <a href="#" class="text-3xl font-mono">CarTrader</a>
+        </nav>
+    </header>
+</template>
