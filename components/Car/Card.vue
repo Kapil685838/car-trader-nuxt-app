e1de05f333f8bdd15f0188f7ae8d8b15c1f1c7ca
Kapil Chaudhary <kc685838@gmail.com>
2024-01-11 00:37:28 +0530

    Added: Card, Cards, SideBar components

---
+++ components/Car/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="shadow border rounded w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+        <div class="flex h-full">
+            <img src="https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1440"
+                alt="" class="w-[300px] h-full" />
+            <div class="p-4 flex flex-col">
+                <div>
+                    <h1 class="text-2xl text-blue-700">
+                        Volvo
+                    </h1>
+                    <p class="text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+                <h1 class="mt-auto text-xl">$39,555</h1>
+            </div>
+        </div>
+    </div>
+</template>
