ad1f2caea7cda89722aafde388445a3f0aa5cef8
Kapil Chaudhary <kc685838@gmail.com>
2024-01-14 20:24:48 +0530

    Updated: Pages title using useHead

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,13 +1,27 @@
+<script setup>
+const route = useRoute();
+
+const toTitleCase = (str) => {
+    return str.replace(/\w\S*/g, (txt) => {
+        return txt.charAt(0).toUpperCase() + txt.substr(1);
+    });
+};
+
+useHead({
+  title: toTitleCase(route.params.name),
+});
+</script>
+
 <template>
-    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16">
-        <div>
-            <CarDetailHero />
+  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16">
+    <div>
+      <CarDetailHero />
 
-            <CarDetailAttributes />
+      <CarDetailAttributes />
 
-            <CarDetailDescription />
+      <CarDetailDescription />
 
-            <CarDetailContact />
-        </div>
+      <CarDetailContact />
     </div>
+  </div>
 </template>
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,11 +1,25 @@
+<script setup>
+const route = useRoute();
+
+const toTitleCase = (str) => {
+  return str.replace(/\w\S*/g, (txt) => {
+    return txt.charAt(0).toUpperCase() + txt.substr(1);
+  });
+};
+
+useHead({
+  title: `${route.params.make ? toTitleCase(route.params.make) : "Cars"} in ${toTitleCase(
+    route.params.city
+  )}`,
+});
+</script>
+
 <template>
-    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16">
-        <div class="mt-32 flex">
+  <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16">
+    <div class="mt-32 flex">
+      <CarSideBar />
 
-            <CarSideBar />
-
-            <NuxtPage />
-
-        </div>
+      <NuxtPage />
     </div>
+  </div>
 </template>
