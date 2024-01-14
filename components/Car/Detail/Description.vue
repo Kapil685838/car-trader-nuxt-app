9defdeed78b7335d5942455db292af289e70a1de
Kapil Chaudhary <kc685838@gmail.com>
2024-01-14 16:09:11 +0530

    Updated: Car Detail Page

---
+++ components/Car/Detail/Description.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="mt-5">
+        <p class="mb-10">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veritatis
+            corporis, ad eveniet accusamus doloribus exercitationem expedita
+            suscipit asperiores doloremque. Iste natus vero iusto blanditiis a cum
+            cumque impedit ab.
+        </p>
+    </div>
+</template>
