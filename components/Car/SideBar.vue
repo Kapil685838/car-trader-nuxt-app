c0ec2eb4c2fa5a2b95e9cc97df6bde5e8fd94633
Kapil Chaudhary <kc685838@gmail.com>
2024-01-15 01:13:49 +0530

    Updated: SideBar location modal

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,24 +1,50 @@
+<script setup>
+const route = useRoute();
+const modal = ref({
+  make: false,
+  location: false,
+  price: false,
+});
+const city = ref("");
+
+const updateModal = (key) => {
+  modal.value[key] = !modal.value[key];
+};
+
+const onChangeLocation = () => {
+  if (!city.value) return;
+  updateModal("location");
+  navigateTo(`/city/${city.value}/car/${route.params.make}`);
+};
+</script>
+
 <template>
-    <div class="shadow border mr-10 z-30 max-w-64">
-        <div class="p-5 flex justify-between gap-12 relative cursor-pointer border-b">
-            <h3>Location</h3>
-            <h3 class="text-blue-400 capitalize">Toronto</h3>
-            <!-- <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-              <input type="text" class="border p-1 rounded">
-              <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">
-                Apply
-              </button>
-            </div> -->
-        </div>
-
-        <div class="p-5 flex justify-between gap-12 relative cursor-pointer border-b">
-            <h3>Make</h3>
-            <h3 class="text-blue-400 capitalize">Toyota</h3>
-        </div>
-
-        <div class="p-5 flex justify-between gap-12 relative cursor-pointer border-b">
-            <h3>Price</h3>
-            <h3 class="text-blue-400 capitalize">Any</h3>
-        </div>
+  <div class="shadow border mr-10 z-30 max-w-64">
+    <div class="p-5 flex justify-between gap-12 relative cursor-pointer border-b">
+      <h3>Location</h3>
+      <h3 @click="updateModal('location')" class="text-blue-400 capitalize">{{ route.params.city }}</h3>
+      <div
+        v-if="modal.location"
+        class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white"
+      >
+        <input v-model="city" type="text" class="border p-1 rounded" />
+        <button
+          @click="onChangeLocation"
+          class="bg-blue-400 w-full mt-2 rounded text-white p-1"
+        >
+          Apply
+        </button>
+      </div>
     </div>
+
+    <div class="p-5 flex justify-between gap-12 relative cursor-pointer border-b">
+      <h3>Make</h3>
+      <h3 class="text-blue-400 capitalize">Toyota</h3>
+    </div>
+
+    <div class="p-5 flex justify-between gap-12 relative cursor-pointer border-b">
+      <h3>Price</h3>
+      <h3 class="text-blue-400 capitalize">Any</h3>
+    </div>
+  </div>
 </template>
