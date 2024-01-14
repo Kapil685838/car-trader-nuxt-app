9e6c95fd6caf127a109c1243d42be899caf6e344
Kapil Chaudhary <kc685838@gmail.com>
2024-01-14 23:49:52 +0530

    Updated: Card content dynamically

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,17 +1,24 @@
+<script setup>
+const props = defineProps({
+  car: Object,
+});
+</script>
+
 <template>
-    <div class="shadow border rounded w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
-        <div class="flex h-full">
-            <img src="https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1440"
-                alt="" class="w-[300px] h-full" />
-            <div class="p-4 flex flex-col">
-                <div>
-                    <h1 class="text-2xl text-blue-700">
-                        Volvo
-                    </h1>
-                    <p class="text-gray-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-                <h1 class="mt-auto text-xl">$39,555</h1>
-            </div>
+  <div class="shadow border rounded w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+  @click="navigateTo(`/car/${car.name}-${car.id}`)"
+  >
+    <div class="flex h-full">
+      <img :src="car.url" alt="" class="w-[300px] h-full" />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
+          <p class="text-gray-700">
+            {{ car.description }}
+          </p>
         </div>
+        <h1 class="mt-auto text-xl">${{ car.price }}</h1>
+      </div>
     </div>
+  </div>
 </template>
