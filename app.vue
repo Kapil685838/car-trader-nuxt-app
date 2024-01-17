98852aa5e986ad82e59e517886e307c164fb0d4b
Kapil Chaudhary <kc685838@gmail.com>
2024-01-17 23:02:05 +0530

    Added: Login page

--- app.vue
+++ app.vue
@@ -3,5 +3,10 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
+
+    <!-- 
+      Currently on 
+        Lecture 36. - Adding the Car Make Filter
+    -->
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@vueuse/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "@vueuse/nuxt",
+    "@nuxtjs/supabase",
+  ],
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false,
+  },
 });
