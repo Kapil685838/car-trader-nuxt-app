25839d35d2c0b6188cbd37b8d82639fd100b02f1
Kapil Chaudhary <kc685838@gmail.com>
2024-01-21 23:13:45 +0530

    Updated: CarAdSelect Component

---
+++ components/Car/MessageCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="even:bg-gray-100 p-4">
+        <h1 class="font-bold text-2xl">Kapil Chaudhary</h1>
+        <p class="text-gray-700">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus aspernatur ipsum delectus dolorem beatae molestiae aliquam fugiat, reprehenderit aliquid sint!
+        </p>
+        <div class="flex items-center mt-4">
+            <p class="text-green-700 mr-10">
+                kapil@email.com
+            </p>
+            <p class="text-blue-500">555-555-5555</p>
+        </div>
+    </div>
+</template>
